4b1d1be7184698f3db11c3af43bf413cf6cb63ea
Mariusz <klimcio@gmail.com>
2025-05-31 21:58:22 +0200

    Speed adjusted to frame rate and clamping movement to screen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use macroquad::prelude::*;
 
+const MOVEMENT_SPEED: f32 = 200.0;
+
 // tell Macroquad which function will be run when application starts, and what will be the window title
 #[macroquad::main("My Shmup")]
 async fn main() {
@@ -8,11 +10,17 @@ async fn main() {
     
     let mut x = screen_center_x;
     let mut y = screen_center_y;
+    let radius = 16.0;
     
-    let speed = 2.0;
+    let mut speed;
     
     loop {
         clear_background(DARKPURPLE);
+
+        // time that passed since the last frame
+        let delta_time = get_frame_time();
+
+        speed = MOVEMENT_SPEED * delta_time;
         
         if is_key_down(KeyCode::Right) {
             x += speed;
@@ -26,8 +34,12 @@ async fn main() {
         if is_key_down(KeyCode::Up) {
             y -= speed;
         }
-    
-        draw_circle(x, y, 16.0, YELLOW);
+
+        // clamp is used to clamp a value between a min and max value
+        x = clamp(x, 0.0 + radius, screen_width() - radius);
+        y = clamp(y, 0.0 + radius, screen_height() - radius);
+        
+        draw_circle(x, y, radius, YELLOW);
         
         // waits until the next frame is available
         next_frame().await
