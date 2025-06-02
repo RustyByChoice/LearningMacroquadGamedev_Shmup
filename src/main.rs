16af9b76f3795e3e9e30f85516cd690978df84b6
Mariusz <klimcio@gmail.com>
2025-06-02 20:03:15 +0200

    Generating random squares that fall from the sky

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use macroquad::prelude::*;
 
+mod shape;
+
+use crate::shape::Shape;
+
 const MOVEMENT_SPEED: f32 = 200.0;
 
 // tell Macroquad which function will be run when application starts, and what will be the window title
@@ -7,40 +11,79 @@ const MOVEMENT_SPEED: f32 = 200.0;
 async fn main() {
     let screen_center_x = screen_width() / 2.0;
     let screen_center_y = screen_height() / 2.0;
+    rand::srand(miniquad::date::now() as u64);
     
-    let mut x = screen_center_x;
-    let mut y = screen_center_y;
-    let radius = 16.0;
+    //let mut x = screen_center_x;
+    //let mut y = screen_center_y;
+    //let radius = 16.0;
     
-    let mut speed;
+    //let mut speed;
     
+    let mut squares: Vec<Shape> = vec![];
+    let mut circle = Shape {
+        size: 32.0,
+        speed: MOVEMENT_SPEED,
+        x: screen_center_x,
+        y: screen_center_y,
+    };
+
     loop {
+        // UPDATE
         clear_background(DARKPURPLE);
 
         // time that passed since the last frame
         let delta_time = get_frame_time();
 
-        speed = MOVEMENT_SPEED * delta_time;
+        circle.speed = MOVEMENT_SPEED * delta_time;
+
+        if rand::gen_range(0, 99) >= 95 {
+            let size = rand::gen_range(16.0, 64.0);
+            squares.push(Shape {
+               size,
+               speed: rand::gen_range(50.0, 150.0),
+               x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+               y: -size,
+            });
+        }
         
+        // move squares down the screen
+        for square in &mut squares {
+            square.y += square.speed * delta_time;
+        }
+        
+        // remove squares outside of screen
+        squares.retain(|square| square.y < screen_height() + square.size);
+                             
         if is_key_down(KeyCode::Right) {
-            x += speed;
+            circle.x += circle.speed;
         }
         if is_key_down(KeyCode::Left) {
-            x -= speed;
+            circle.x -= circle.speed;
         }
         if is_key_down(KeyCode::Down) {
-            y += speed;
+            circle.y += circle.speed;
         }
         if is_key_down(KeyCode::Up) {
-            y -= speed;
+            circle.y -= circle.speed;
         }
 
         // clamp is used to clamp a value between a min and max value
-        x = clamp(x, 0.0 + radius, screen_width() - radius);
-        y = clamp(y, 0.0 + radius, screen_height() - radius);
+        circle.x = clamp(circle.x, 0.0 + circle.size, screen_width() - circle.size);
+        circle.y = clamp(circle.y, 0.0 + circle.size, screen_height() - circle.size);
         
-        draw_circle(x, y, radius, YELLOW);
+        // DRAW
         
+        draw_circle(circle.x, circle.y, circle.size, YELLOW);
+        
+        for square in &squares {
+            draw_rectangle(
+                square.x - square.size / 2.0,
+                square.y - square.size / 2.0,
+                square.size,
+                square.size,
+                GREEN,
+            );
+        }
         // waits until the next frame is available
         next_frame().await
     }
---
+++ src/shape.rs
@@ -0,0 +1,7 @@
+pub struct Shape {
+    pub size: f32,
+    pub speed: f32,
+    pub x: f32,
+    pub y: f32,
+}
+
